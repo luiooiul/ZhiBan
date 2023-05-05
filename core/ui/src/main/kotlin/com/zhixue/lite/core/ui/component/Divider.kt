2d1794ffd63138ecfeb26f2ae0a8a505f54eb343
luiooiul <mail@luiooiul.com>
2023-05-05 20:55:56 +0800

    增添分割线组件

---
+++ core/ui/src/main/kotlin/com/zhixue/lite/core/ui/component/Divider.kt
@@ -0,0 +1,29 @@
+package com.zhixue.lite.core.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.zhixue.lite.core.ui.theme.Theme
+
+@Composable
+fun HorizontalDivider(
+    modifier: Modifier = Modifier,
+    height: Dp = 1.dp,
+    color: Color = Theme.colors.surface,
+    spacing: Dp = 0.dp
+) {
+    Spacer(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(height)
+            .padding(horizontal = spacing)
+            .background(color)
+    )
+}
