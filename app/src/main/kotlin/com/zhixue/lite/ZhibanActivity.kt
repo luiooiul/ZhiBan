4ba9ec7100ebfa1108d4e15d42b4efa27424d85f
luiooiul <mail@luiooiul.com>
2023-05-09 00:55:52 +0800

    去除初始屏幕淡出动画

--- app/src/main/kotlin/com/zhixue/lite/ZhibanActivity.kt
+++ app/src/main/kotlin/com/zhixue/lite/ZhibanActivity.kt
@@ -1,14 +1,11 @@
 package com.zhixue.lite
 
-import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.DisposableEffect
-import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
@@ -28,15 +25,6 @@ class ZhibanActivity : ComponentActivity() {
 
         splashScreen.setKeepOnScreenCondition {
             _viewModel.loginState == LoginState.Loading
-        }
-        splashScreen.setOnExitAnimationListener { provider ->
-            ObjectAnimator.ofFloat(provider.view, View.ALPHA, 1f, 0f)
-                .apply {
-                    doOnEnd { provider.remove() }
-                }
-                .also { animator ->
-                    animator.start()
-                }
         }
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
