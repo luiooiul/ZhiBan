0572bc6b8e348f872b84f310e2173e1eb4ae5df3
luiooiul <mail@luiooiul.com>
2023-05-15 17:31:23 +0800

    禁用缓存

--- app/src/main/kotlin/com/zhixue/lite/ZhibanApplication.kt
+++ app/src/main/kotlin/com/zhixue/lite/ZhibanApplication.kt
@@ -3,6 +3,7 @@ package com.zhixue.lite
 import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
+import coil.request.CachePolicy
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -11,6 +12,8 @@ class ZhibanApplication : Application(), ImageLoaderFactory {
     override fun newImageLoader(): ImageLoader {
         return ImageLoader(this).newBuilder()
             .crossfade(true)
+            .diskCachePolicy(CachePolicy.DISABLED)
+            .memoryCachePolicy(CachePolicy.DISABLED)
             .build()
     }
 }
