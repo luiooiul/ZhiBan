cb9e4c85cbce884f71e8b37380c7f43b6aaed875
luiooiul <mail@luiooiul.com>
2023-05-27 18:06:10 +0800

    修复修改密码后登录过期问题

--- feature/modify/src/main/kotlin/com/zhixue/lite/feature/modify/ModifyViewModel.kt
+++ feature/modify/src/main/kotlin/com/zhixue/lite/feature/modify/ModifyViewModel.kt
@@ -5,10 +5,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.zhixue.lite.core.data.repository.LoginRepository
 import com.zhixue.lite.core.data.repository.ModifyRepository
 import com.zhixue.lite.core.data.repository.UserRepository
 import com.zhixue.lite.core.domain.EncryptPasswordUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
@@ -16,6 +18,7 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.seconds
 
 data class ModifyUiState(
     val message: String = "",
@@ -25,6 +28,7 @@ data class ModifyUiState(
 @HiltViewModel
 class ModifyViewModel @Inject constructor(
     private val userRepository: UserRepository,
+    private val loginRepository: LoginRepository,
     private val modifyRepository: ModifyRepository,
     private val encryptPasswordUseCase: EncryptPasswordUseCase
 ) : ViewModel() {
@@ -64,10 +68,11 @@ class ModifyViewModel @Inject constructor(
                 checkInputAvailable()
                 isModifying.value = true
                 modifyRepository.modifyPassword(originPassword, newPassword)
-                userRepository.storeUser(
-                    username = userRepository.userData.first().username,
-                    password = encryptPasswordUseCase(newPassword.reversed())
-                )
+                val username = userRepository.userData.first().username
+                val password = encryptPasswordUseCase(newPassword.reversed())
+                userRepository.storeUser(username, password)
+                delay(1.seconds)
+                loginRepository.login(username, password)
             }.onSuccess {
                 message.value = "修改成功"
                 isModifying.value = false
