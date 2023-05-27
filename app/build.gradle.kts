8b055db32945ca5e61cecad3b30fa197bce22f78
luiooiul <mail@luiooiul.com>
2023-05-27 23:37:11 +0800

    更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.zhixue.lite"
         minSdk = 21
         targetSdk = 33
-        versionCode = 5
-        versionName = "1.0-beta01"
+        versionCode = 6
+        versionName = "1.0-beta02"
 
         resourceConfigurations.add("en")
     }
