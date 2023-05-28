aed9f13452104cd01f63169b79b3b6c7ca1d3064
luiooiul <mail@luiooiul.com>
2023-05-28 13:00:37 +0800

    支持原卷离线查看

--- app/src/main/kotlin/com/zhixue/lite/ZhibanApplication.kt
+++ app/src/main/kotlin/com/zhixue/lite/ZhibanApplication.kt
@@ -3,7 +3,6 @@ package com.zhixue.lite
 import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
-import coil.request.CachePolicy
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -12,7 +11,7 @@ class ZhibanApplication : Application(), ImageLoaderFactory {
     override fun newImageLoader(): ImageLoader {
         return ImageLoader(this).newBuilder()
             .crossfade(true)
-            .diskCachePolicy(CachePolicy.DISABLED)
+            .respectCacheHeaders(false)
             .build()
     }
 }
