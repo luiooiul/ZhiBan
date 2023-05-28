45a8a94a54241c06d260c709fafb4c3975aa9fdc
luiooiul <mail@luiooiul.com>
2023-05-28 09:58:20 +0800

    更新依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.library") version "8.0.0" apply false
-    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.2" apply false
+    id("com.android.application") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21" apply false
     id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
