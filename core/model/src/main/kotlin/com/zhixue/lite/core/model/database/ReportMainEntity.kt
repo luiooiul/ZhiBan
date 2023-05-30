1a79368c16ef1a326e1f0dbe1fb935f28ee749d9
luiooiul <mail@luiooiul.com>
2023-05-30 10:45:11 +0800

    移除无用import

--- core/model/src/main/kotlin/com/zhixue/lite/core/model/database/ReportMainEntity.kt
+++ core/model/src/main/kotlin/com/zhixue/lite/core/model/database/ReportMainEntity.kt
@@ -4,7 +4,6 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.zhixue.lite.core.common.Json
 import com.zhixue.lite.core.model.data.ReportMain
-import kotlinx.serialization.decodeFromString
 
 @Entity
 data class ReportMainEntity(
--- core/model/src/main/kotlin/com/zhixue/lite/core/model/network/CheckSheetResponse.kt
+++ core/model/src/main/kotlin/com/zhixue/lite/core/model/network/CheckSheetResponse.kt
@@ -4,7 +4,6 @@ import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.builtins.serializer
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.JsonDecoder
